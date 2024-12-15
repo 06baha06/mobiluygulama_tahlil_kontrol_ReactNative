6c3ba8d994bb21d662f143f66a93cf9d2c752e46
06baha06 <104167309+06baha06@users.noreply.github.com>
2024-12-16 00:35:41 +0300

    login sayfasi baslangic

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable, Image, } from 'react-native';
 
 export default function App() {
+
+  const [name,setName] = useState("")
+  const [lastName,setLastName] = useState("")
+  const [result,setResult] = useState("")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+      <Text>Welcome {result}</Text>
+      
+      <Text>Name</Text>
+      <TextInput
+        placeholder='Enter Your Name'
+        style={styles.textInputStyle}
+        onChangeText={setName}
+        value={name}
+      ></TextInput>
+
+      <Text>Last Name</Text>
+      <TextInput
+        placeholder='Enter Your Last Name'
+        style={styles.textInputStyle}
+        onChangeText={setLastName}
+        value={lastName}
+      ></TextInput>
+
+      <Pressable
+        onPress={() => setResult(name + " " + lastName)}
+        style={({pressed}) => [
+          styles.button,
+          {
+            backgroundColor: pressed ? 'gray' : 'blue'
+          }
+        ]}
+      >
+        <Text style={styles.buttonText}>Save</Text>
+      </Pressable>
+
+
     </View>
   );
 }
@@ -17,4 +53,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInputStyle:{
+    borderWidth:1,
+    width:'80%',
+    height:50,
+    borderRadius:10,
+    marginVertical:10,
+    textAlign:'center',
+    color:'blue',
+    fontWeight:'bold'
+  },
+  button:{
+    width:'80%', 
+    height:50, 
+    borderRadius:10,
+    alignItems:'center', 
+    justifyContent:'center',  
+    backgroundColor: 'lightblue'
+  },
+  buttonText:{
+    fontWeight:'bold',
+    color:'white'
+  }
 });
